f574f38c7464e3e69f869ce35ab6a995b3763c19
Levin Keller <levin@coyno.com>
2015-05-12 17:10:51 +0200

    Adding worker logic into the package.

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-module.exports = {
-  bufferSize: process.env.ADDRESS_BUFFER_SIZE || 200
-};
+
+
+process.env.ADDRESS_BUFFER_SIZE = process.env.ADDRESS_BUFFER_SIZE || 200;
+
+process.env.MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:3001/meteor';
